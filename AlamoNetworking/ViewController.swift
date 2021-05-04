07d6bd08993bde3c53b9fb5c42d9e91f88808aad
Stanislav <te100ff@inbox.ru>
2021-05-04 22:21:33 +0300

    Initial Commit

---
+++ AlamoNetworking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlamoNetworking
+//
+//  Created by Stanislav Testov on 04.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
