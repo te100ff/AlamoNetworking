0d1ff20d82e08d489321db6acba341f3d9d04547
Stanislav <te100ff@inbox.ru>
2021-05-05 10:27:07 +0300

    Storyboard

---
+++ AlamoNetworking/CharacterCell.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterCollectionViewCell.swift
+//  AlamoNetworking
+//
+//  Created by Stanislav Testov on 05.05.2021.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var characterLabel: UILabel!
+    
+}
